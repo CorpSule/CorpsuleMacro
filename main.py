eb34ca866438ed53cbb809ca9136649557515c2a
CorpSule <orb747z@gmail.com>
2026-07-25 19:23:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from utils.vision import Vision
 from utils.actions import Actions
 from utils.webhook import DiscordWebhook
 
-CURRENT_VERSION = "1.01"
+CURRENT_VERSION = "1.03"
 # Your GitHub Repository Raw Version URL
 UPDATE_CHECK_URL = "https://raw.githubusercontent.com/CorpSule/CorpsuleMacro/main/version.json"
 
